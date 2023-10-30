aa4d4974991e121a5404bd43b8b21c15cc5f1106
cldiegoj <145816958+cldiegoj@users.noreply.github.com>
2023-10-30 18:33:50 -0500

    Primer Commit

---
+++ src/main/java/com/mycompany/integrador/Integrador.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.integrador;
+
+/**
+ *
+ * @author djcor
+ */
+public class Integrador {
+
+    public static void main(String[] args) {
+        System.out.println("xd");
+    }
+}
