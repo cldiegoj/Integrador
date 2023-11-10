cd593b24c213ad6e81f93643f5092a973de43109
cldiegoj <145816958+cldiegoj@users.noreply.github.com>
2023-11-09 21:31:08 -0500

    categoria

---
+++ src/Modelo/Categoria.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author djcor
+ */
+public class Categoria {
+
+    private String idCategoria;
+    private String nombre;
+    private String descripcion;
+
+    public Categoria() {
+    }
+
+    public Categoria(String idCategoria, String nombre, String descripcion) {
+        this.idCategoria = idCategoria;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
+
+    public String getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(String idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+}
