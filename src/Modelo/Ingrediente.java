f7d83ea611a0884057d6e7015e0e4b4d6f51dfc0
cldiegoj <145816958+cldiegoj@users.noreply.github.com>
2023-12-01 15:18:11 -0500

    Commit 1/12.5

---
+++ src/Modelo/Ingrediente.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author djcor
+ */
+public class Ingrediente {
+
+    private int idProducto;
+    private int idInsumo;
+    private int cantidad;
+    private String nombreproducto;
+    private String nombreinsumo;
+
+    public Ingrediente() {
+    }
+
+    public int getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(int idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public int getIdInsumo() {
+        return idInsumo;
+    }
+
+    public void setIdInsumo(int idInsumo) {
+        this.idInsumo = idInsumo;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public String getNombreproducto() {
+        return nombreproducto;
+    }
+
+    public void setNombreproducto(String nombreproducto) {
+        this.nombreproducto = nombreproducto;
+    }
+
+    public String getNombreinsumo() {
+        return nombreinsumo;
+    }
+
+    public void setNombreinsumo(String nombreinsumo) {
+        this.nombreinsumo = nombreinsumo;
+    }
+
+}
