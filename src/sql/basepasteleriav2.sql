514a39ba3b6b8ad00d53ef15200b9fbd5b051c40
cldiegoj <145816958+cldiegoj@users.noreply.github.com>
2023-11-30 20:28:49 -0500

    USUARIO

--- src/sql/basepasteleriav2.sql
+++ src/sql/basepasteleriav2.sql
@@ -1,31 +1,10 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 12-11-2023 a las 22:12:08
--- Versión del servidor: 10.4.28-MariaDB
--- Versión de PHP: 8.2.4
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de datos: `basepasteleria`
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `cargo`
---
 
 
 CREATE DATABASE  IF NOT EXISTS `basepasteleria`;
@@ -37,20 +16,13 @@ CREATE TABLE `cargo` (
   `car_nom` varchar(45) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Volcado de datos para la tabla `cargo`
---
+
 
 INSERT INTO `cargo` (`car_cod`, `car_nom`) VALUES
 (1, 'ADMINISTRADOR'),
 (2, 'VENDEDOR'),
 (3, 'ENCARGADO DE ALMACEN');
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `categoria`
---
 
 CREATE TABLE `categoria` (
   `cat_cod` char(5) NOT NULL,
@@ -58,11 +30,7 @@ CREATE TABLE `categoria` (
   `cat_des` varchar(45) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
 
---
--- Estructura de tabla para la tabla `cliente`
---
 
 CREATE TABLE `cliente` (
   `cli_cod` int(11) NOT NULL,
@@ -72,7 +40,6 @@ CREATE TABLE `cliente` (
   `cli_dir` varchar(45) DEFAULT NULL,
   `cli_cor` varchar(45) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
--- --------------------------------------------------------
 
 ALTER TABLE `cliente`
   ADD PRIMARY KEY (`cli_cod`);
@@ -81,21 +48,12 @@ ALTER TABLE `cliente`
   MODIFY `cli_cod` int(11) NOT NULL AUTO_INCREMENT;
 
 
---
--- Estructura de tabla para la tabla `detalle_recibo`
---
-
 CREATE TABLE `detalle_recibo` (
   `reb_cod` char(5) NOT NULL,
   `prod_can` int(11) DEFAULT NULL,
   `prod_cod` char(5) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `ingredientes`
---
 
 CREATE TABLE `ingredientes` (
   `prod_cod` char(5) NOT NULL,
@@ -103,11 +61,6 @@ CREATE TABLE `ingredientes` (
   `ing_can` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `insumos`
---
 
 CREATE TABLE `insumos` (
   `ins_cod` char(5) NOT NULL,
@@ -117,11 +70,6 @@ CREATE TABLE `insumos` (
   `pro_cod` char(5) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `pago`
---
 
 CREATE TABLE `pago` (
   `pag_cod` char(5) NOT NULL,
@@ -132,11 +80,6 @@ CREATE TABLE `pago` (
   `reb_cod` char(5) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `producto`
---
 
 CREATE TABLE `producto` (
   `prod_cod` char(5) NOT NULL,
@@ -147,11 +90,6 @@ CREATE TABLE `producto` (
   `cat_cod` char(5) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `proveedor`
---
 
 CREATE TABLE `proveedor` (
   `pro_cod` char(5) NOT NULL,
@@ -160,23 +98,16 @@ CREATE TABLE `proveedor` (
   `pro_ruc` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
 
---
--- Estructura de tabla para la tabla `recibo`
---
 
 CREATE TABLE `recibo` (
   `reb_cod` char(5) NOT NULL,
   `reb_fec` date DEFAULT NULL,
-  `cli_cod` char(5) DEFAULT NULL
+  `cli_cod` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
 
---
--- Estructura de tabla para la tabla `usuario`
---
+
 
 CREATE TABLE `usuario` (
   `usr_cod` int(11) NOT NULL,
@@ -189,148 +120,89 @@ CREATE TABLE `usuario` (
   `car_cod` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Volcado de datos para la tabla `usuario`
---
+
 
 INSERT INTO `usuario` (`usr_cod`, `usr_name`, `usr_pass`, `usr_nom`, `usr_ape`, `usr_telf`, `usr_status`, `car_cod`) VALUES
 (1, 'kfernandezs', 'Password$1', 'Kevin', 'Fernandez Salas', '914233345', 1, 1),
 (3, 'jfernandezs', 'Password$1', 'Jean', 'Fernandez Salas', '987654321', 1, 1);
 
---
--- Índices para tablas volcadas
---
 
---
--- Indices de la tabla `cargo`
---
 ALTER TABLE `cargo`
   ADD PRIMARY KEY (`car_cod`);
 
---
--- Indices de la tabla `categoria`
---
+
 ALTER TABLE `categoria`
   ADD PRIMARY KEY (`cat_cod`);
 
---
--- Indices de la tabla `cliente`
---
-
---
--- Indices de la tabla `detalle_recibo`
---
 ALTER TABLE `detalle_recibo`
   ADD PRIMARY KEY (`reb_cod`),
   ADD KEY `fk_detalle_producto` (`prod_cod`);
 
---
--- Indices de la tabla `ingredientes`
---
 ALTER TABLE `ingredientes`
   ADD PRIMARY KEY (`prod_cod`),
   ADD KEY `fk_ingredientes_insumos` (`ins_cod`);
 
---
--- Indices de la tabla `insumos`
---
 ALTER TABLE `insumos`
   ADD PRIMARY KEY (`ins_cod`),
   ADD KEY `fk_insumos_proveedor` (`pro_cod`);
 
---
--- Indices de la tabla `pago`
---
 ALTER TABLE `pago`
   ADD PRIMARY KEY (`pag_cod`),
   ADD KEY `fk_pago_recibo` (`reb_cod`);
 
---
--- Indices de la tabla `producto`
---
+
 ALTER TABLE `producto`
   ADD PRIMARY KEY (`prod_cod`),
   ADD KEY `fk_producto_categoria` (`cat_cod`);
 
---
--- Indices de la tabla `proveedor`
---
 ALTER TABLE `proveedor`
   ADD PRIMARY KEY (`pro_cod`);
 
---
--- Indices de la tabla `recibo`
---
+
 ALTER TABLE `recibo`
   ADD PRIMARY KEY (`reb_cod`),
   ADD KEY `fk_recibo_cliente` (`cli_cod`);
 
---
--- Indices de la tabla `usuario`
---
+
 ALTER TABLE `usuario`
   ADD PRIMARY KEY (`usr_cod`),
   ADD KEY `fk_usuario_cargo` (`car_cod`);
 
---
--- AUTO_INCREMENT de las tablas volcadas
---
 
---
--- AUTO_INCREMENT de la tabla `usuario`
---
+
+
 ALTER TABLE `usuario`
   MODIFY `usr_cod` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
---
--- Restricciones para tablas volcadas
---
-
---
--- Filtros para la tabla `detalle_recibo`
---
 ALTER TABLE `detalle_recibo`
   ADD CONSTRAINT `fk_detalle_producto` FOREIGN KEY (`prod_cod`) REFERENCES `producto` (`prod_cod`),
   ADD CONSTRAINT `fk_detalle_recibo` FOREIGN KEY (`reb_cod`) REFERENCES `recibo` (`reb_cod`);
 
---
--- Filtros para la tabla `ingredientes`
---
+
 ALTER TABLE `ingredientes`
   ADD CONSTRAINT `fk_ingredientes_insumos` FOREIGN KEY (`ins_cod`) REFERENCES `insumos` (`ins_cod`),
   ADD CONSTRAINT `fk_ingredientes_producto` FOREIGN KEY (`prod_cod`) REFERENCES `producto` (`prod_cod`);
 
---
--- Filtros para la tabla `insumos`
---
+
 ALTER TABLE `insumos`
   ADD CONSTRAINT `fk_insumos_proveedor` FOREIGN KEY (`pro_cod`) REFERENCES `proveedor` (`pro_cod`);
 
---
--- Filtros para la tabla `pago`
---
+
 ALTER TABLE `pago`
   ADD CONSTRAINT `fk_pago_recibo` FOREIGN KEY (`reb_cod`) REFERENCES `recibo` (`reb_cod`);
 
---
--- Filtros para la tabla `producto`
---
+
 ALTER TABLE `producto`
   ADD CONSTRAINT `fk_producto_categoria` FOREIGN KEY (`cat_cod`) REFERENCES `categoria` (`cat_cod`);
 
---
--- Filtros para la tabla `recibo`
---
+
+
 ALTER TABLE `recibo`
   ADD CONSTRAINT `fk_recibo_cliente` FOREIGN KEY (`cli_cod`) REFERENCES `cliente` (`cli_cod`);
 
---
--- Filtros para la tabla `usuario`
---
+
 ALTER TABLE `usuario`
   ADD CONSTRAINT `fk_usuario_cargo` FOREIGN KEY (`car_cod`) REFERENCES `cargo` (`car_cod`);
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
